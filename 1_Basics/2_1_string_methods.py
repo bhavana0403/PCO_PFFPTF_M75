cdce6ac30b73addf3f135993d66ecf10bdc8783c
bhavana0403 <bhavanabm97@gmail.com>
2024-07-01 19:58:23 +0530

    strings

---
+++ 1_Basics/2_1_string_methods.py
@@ -0,0 +1,84 @@
+# string methods
+
+# 1) upper
+"""
+-> It is a method which is used to convert lowercase characters to uppercase characters
+   and print it.
+-> Syntax:
+        Var.upper()
+-> String methods do not modify the original string
+-> The return type of upper is a string
+"""
+
+# st = 'hello'
+# print(st.upper())   # HELLO
+# print(st)           # hello
+#
+# print('HeLLO WoRlD 123@#$'.upper())     # HELLO WORLD 123@#$
+
+##################################################################################
+
+# 2) lower()
+"""
+-> It is a method which is used to convert all the uppercase characters to 
+   lowercase characters
+-> Syntax:
+        Var.lower()
+-> The return type of lower is a string
+"""
+
+# print('HeLLO WoRlD 123@#$'.lower())     # hello world 123@#$
+
+#####################################################################################
+
+# 3) swapcase
+"""
+-> It is used to convert uppercase to lowercase and lowercase to uppercase
+-> Syntax:
+        Var.swapcase()
+-> The retun type of swapcase is a string
+"""
+
+# st = "heLLo WorLd"
+# print(st.swapcase())    # HEllO wORlD
+
+#####################################################################################
+
+# 4) title()
+"""
+-> It is used to convert the given string to title format
+-> Syntax:
+        Var.title()
+-> The return type of title is a string
+"""
+
+# book = "the stories of mahabharata"
+# print(book.title())     # The Stories Of Mahabharata
+#
+# book = "the 4tune teller"
+# print(book.title())     # The 4Tune Teller
+#
+# print('ONE MAN ARMY'.title())   # One Man Army
+
+####################################################################################
+
+# 5) capitalize
+"""
+-> It is used to convert the first character of a string to uppercase and all the 
+   other alphabets to lowercase
+-> Syntax:
+        Var.capitalize()
+-> The return type of capitalize is a string
+"""
+
+# book = "the stories of mahabharata"
+# print(book.capitalize())    # The stories of mahabharata
+#
+# book = '3 mistakes of my life'
+# print(book.capitalize())    # 3 mistakes of my life
+#
+# book = '4tune teller'
+# print(book.capitalize())    # 4tune teller
+
+#####################################################################################
+
