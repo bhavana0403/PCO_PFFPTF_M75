d7c3c11223749055590914071601e7e65fc2b964
bhavana0403 <bhavanabm97@gmail.com>
2024-07-01 20:37:27 +0530

    strings

--- 1_Basics/2_1_string_methods.py
+++ 1_Basics/2_1_string_methods.py
@@ -82,3 +82,107 @@
 
 #####################################################################################
 
+# 6) isupper()
+"""
+-> It is used to check whether all the alphabets in the string are uppercase alphabets
+-> Syntax:
+        Var.isupper()
+-> The return type of isupper is boolean
+"""
+
+# st = 'HELLO'
+# print(st.isupper())     # True
+#
+# print('HELLO WORLD'.isupper())      # True
+#
+# print('HELLO 123@')     # True
+#
+# print('HELLo'.isupper())    # False
+
+#################################################################################
+
+ # 7) islower
+"""
+ 
+"""
+st = 'hello world'
+print(st.islower())    # True
+
+#################################################################################
+
+# 8) istitle
+"""
+
+"""
+
+print('THE ONE MAN ARMY'.istitle())     # False
+
+print('One Man Army'.istitle())     # True
+
+###################################################################################
+
+# 9) isalpha()
+"""
+-> It is used to check whether the string has only alphabets
+-> Syntax:
+        Var.isalpha()
+-> The return type of isalpha is boolean
+"""
+
+st = 'python'
+print(st.isalpha())     # True
+
+print('PYTHON'.isalpha())   # True
+
+print('hello WORLD'.isalpha())      # False
+
+print('h@I'.isalpha())      # False
+
+##################################################################################
+
+# 10) isdigit()
+"""
+
+"""
+
+st = '9845 123 654'
+print(st.isdigit())     # FAlse
+
+ph_no = '8123458765'
+print(ph_no.isdigit())      # True
+
+##################################################################################
+
+# 11) isalnum
+"""
+
+"""
+
+print('APPle 120'.isalnum())      # False
+print('APpLe120'.isalnum())         # True
+print('APPLE'.isalnum())        # True
+print('apple'.isalnum())        # True
+print('1234'.isalnum())         # True
+
+###################################################################################
+
+# 12) startswith
+"""
+-> It is used to check whether a string is starting with a specified substring
+-> Syntax:
+        Var.startswith(substring) 
+-> The return type of startswith is 
+"""
+
+print('hello universe'.startswith('he'))    # True
+print('hello universe'.startswith('H'))     # False
+
+print('python class'.startswith('pYTHOn'))  # False
+
+####################################################################################
+
+# 13) endswith
+
+#################################################################################
+
+# 14)
