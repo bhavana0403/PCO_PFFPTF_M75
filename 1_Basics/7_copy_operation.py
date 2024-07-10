a6f3b1171fb5d89ac557ee2a78fb6b04c69284fa
bhavana0403 <bhavanabm97@gmail.com>
2024-07-10 19:59:59 +0530

    copy operation, list methods

--- 1_Basics/7_copy_operation.py
+++ 1_Basics/7_copy_operation.py
@@ -16,18 +16,57 @@
 -> In case of general copy, changes wrt one variable will change other variable
 """
 
-nums = [[1, 2], 3, 4]
-gen_copy = nums
+# nums = [[1, 2], 3, 4]
+# gen_copy = nums
+#
+# nums[0][1] = 22
+# print(nums)         # [[1, 22], 3, 4]
+# print(gen_copy)     # [[1, 22], 3, 4]
+#
+# nums[1] = 33
+# print(nums)         # [[1, 22], 33, 4]
+# print(gen_copy)     # [[1, 22], 33, 4]
 
-nums[0][1] = 22
-print(nums)         # [[1, 22], 3, 4]
-print(gen_copy)     # [[1, 22], 3, 4]
+########################################################################################
 
-nums[1] = 33
-print(nums)         # [[1, 22], 33, 4]
-print(gen_copy)     # [[1, 22], 33, 4]
+# 2) shallow copy operation
+"""
+-> In case of shallow copy, value space of one variable will be copied to another variable
+-> Syntax:
+            dest_var = source_var.copy()
+-> Here, changes wrt one variable will not affect another variable in case of linear
+   list. but, in case of nested list, chanegs wrt one variable will affect another
+   variable
+"""
 
+# nums = [[1, 2], 3, 4]
+# shallow_copy = nums.copy()
 
+# nums[2] = 44
+# print(nums)             # [[1, 2], 3, 44]
+# print(shallow_copy)     # [[1, 2], 3, 4]
+#
+# nums[0][0] = 11
+# print(nums)             # [[11, 2], 3, 44]
+# print(shallow_copy)     # [[11, 2], 3, 4]
 
+######################################################################################
 
+# 3) Deep Copy
+"""
+-> In deep copy, the entire values of one variable will be copied to another variable 
+   with different address
+-> Syntax:
+            import copy
+            dest_var = copy.deepcopy(source_var)
+-> Here, changes wrt one variable will not affect another variable
+"""
+# import copy
+# nums = [[1, 2], 3, 4]
+# deep_copy = copy.deepcopy(nums)
+#
+# nums[0][1] = 22
+# print(nums)         # [[1, 22], 3, 4]
+# print(deep_copy)    # [[1, 2], 3, 4]
 
+#######################################################################################
