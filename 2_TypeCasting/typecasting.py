4f4f2b7fb263e91f89c3b2f4614dc66f19b03611
bhavana0403 <bhavanabm97@gmail.com>
2024-07-17 20:00:52 +0530

    typecasting

---
+++ 2_TypeCasting/typecasting.py
@@ -0,0 +1,137 @@
+# TypeCasting
+"""
+-> Converting one datatype to another
+-> Syntax:
+            dest_var = dest_datatype(source_var)
+"""
+
+# 1) Typecasting of integer to other datatypes
+
+num = 43
+
+print(float(num))       # 43.0
+
+print(complex(num))     # (43+0j)
+
+print(bool(num))       # True
+print(bool(0))          # False
+
+print(str(num))         # '43'
+
+# print(list(num))        # TypeError
+
+# print(tuple(num))       # TypeError
+
+# print(set(num))         # TypeError
+
+# print(dict(num))        # TypeError
+
+""" We can not typecast integer to multivalue datatypes except string"""
+##########################################################################################
+
+# 2) Typecasting of float to other datatypes
+
+num = 12.77
+
+print(int(num))     # 12
+
+print(complex(num))     # (12.77+0j)
+
+print(bool(num))        # True
+print(bool(0.0))        # False
+
+print(str(num))         # '12.77'
+print(len(str(num)))    # 5
+
+# print(list(num))        # TypeError
+
+# print(tuple(num))       # TypeError
+
+# print(set(num))         # TypeError
+
+# print(dict(num))        # TypeError
+
+#######################################################################################
+
+# 3) Typecasting of complex to otehr datatypes
+
+num = 10-5j
+
+# print(int(num))         # TypeError
+
+# print(float(num))       # TypeError
+
+print(bool(num))        # True
+print(bool(0j))         # False
+
+print(str(num))         # '(10-5j)'
+
+# print(list(num))        # TypeError
+
+# print(tuple(num))       # TypeError
+
+# print(set(num))         # TypeError
+
+# print(dict(num))         # TypeError
+"""complex can not be converted to other datatypes except string and boolean"""
+######################################################################################
+
+# 4) Typecasting of boolean to other datatypes
+
+a = True
+b = False
+
+print(int(a))           # 1
+print(int(b))           # 0
+
+print(float(a))         # 1.0
+print(float(b))         # 0.0
+
+print(complex(a))       # (1+0j)
+print(complex(b))       # 0j
+
+print(str(a))           # 'True'
+print(str(b))           # 'False'
+
+# print(list(a))          # TypeError
+
+# print(tuple(a))         # TypeError
+
+# print(set(a))           # TypeError
+
+# print(dict(a))          # TypeError
+
+########################################################################################
+
+# 5) Typecasting of string to other datatypes
+
+a = 'hai'
+b = '87'
+
+# print(int(a))           # ValueError
+print(int(b))           # 87
+# print(int('23.78'))     # ValueError
+
+print(float(b))         # 87.0
+print(float('23.78'))   # 23.78
+
+print(complex(b))       # (87+0j)
+print(complex('24.86')) # (24.86+0j)
+print(complex('3+7j'))  # (3+7j)
+
+print(bool(b))          # True
+print(bool('True'))     # True
+print(bool('False'))    # True
+print(bool(''))         # False
+
+print(list(a))          # ['h', 'a', 'i']
+
+print(tuple(a))         # ('h', 'a', 'i')
+
+print(set(a))           # {'h', 'i', 'a'}
+
+# print(dict(a))          # ValueError
+# print(dict("{'a': 1}"))     # ValueError
+
+########################################################################################
+
