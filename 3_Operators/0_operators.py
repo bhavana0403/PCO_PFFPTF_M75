7a0eb139a80be4b791922d6ebb74d1d9177c6fb9
bhavana0403 <bhavanabm97@gmail.com>
2024-07-17 20:56:23 +0530

    slicing

---
+++ 3_Operators/0_operators.py
@@ -0,0 +1,13 @@
+# Operators
+"""
+-> These are the special symbols which are used to perform some specific operations
+-> To perform any operation we need operator and operand (values)
+-> Classified into 7 types
+    1) Arithmetic Operator
+    2) Relational Operator
+    3) Logical Operator
+    4) Bitwise Operator
+    5) Assignment Operator
+    6) Membership Operator
+    7) Identity Operator
+"""
