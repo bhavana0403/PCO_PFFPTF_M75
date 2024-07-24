b83aeae68551420ed6c0d6f1af5431069c45cd20
bhavana0403 <bhavanabm97@gmail.com>
2024-07-24 20:09:00 +0530

    conditional statements

---
+++ 5_Control_Statements/4_nested_if_statement.py
@@ -0,0 +1,93 @@
+# nested if
+"""
+-> Syntax:
+            if <cond1>:
+                if <cond2>:
+                    TSB2
+                else:
+                    FSB2
+            else:
+                FSB1
+
+"""
+
+"""
+1) WAP to check if a given list has a mid value, if it has a mid value then check if it 
+   is a integer.  
+"""
+
+# l = [1, 'a', 3, 4, 5]
+# if len(l) % 2 != 0:
+#     print('The given list has a mid value')
+#     mid_index = len(l) // 2     # 1
+#     if type(l[mid_index]) == int:
+#         print('Mid value is an integer')
+#     else:
+#         print('Mid value is not an integer')
+# else:
+#     print('The given list does not have a mid value')
+
+"""
+2) WAP to check if the given data is an integer, if it is an integer, check if it is
+   divisible by 8
+"""
+
+# data = 88.97
+# if type(data) == int:
+#     print('The given data is an integer')
+#     if data % 8 == 0:
+#         print('The given integer is divisible by 8')
+#     else:
+#         print('Not divisible by 8')
+# else:
+#     print('The given data is not an integer')
+
+# 3) WAP to check if a user is able to log in to his instagram account
+
+# original_username = "alexjoseph"
+# original_password = "alex123"
+# username = input('Enter the username : ')
+# if username == original_username:
+#     password = input('Enter the password : ')
+#     if password == original_password:
+#         print('Login Successful')
+#     else:
+#         print('Incorrect Password')
+# else:
+#     print('Invalid Username')
+
+"""
+4)  WAP to take input for 6 subject marks of a student, check if the student has 
+    scored more than 35 in all the subjects, if he has scored more than 35 in all
+    subjects calculate the total marks and print the result along with percentage.
+    if the student has scored less than 35 in any one of teh subject then print FAIL
+    result -> distinction, first class, second class, pass class, FAIL
+"""
+
+# 5) WAP to find the second highest among 3 distinct numbers
+
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+# if num1 > num2 and num1 > num3:
+#     if num2 > num3:
+#         print('num2 is second highest')
+#     else:
+#         print('num3 is second highest')
+# elif num2 > num3:
+#     if num3 > num1:
+#         print('num3 is second highest')
+#     else:
+#         print('num1 is second highest')
+# else:
+#     if num1 > num2:
+#         print('num1 is second highest')
+#     else:
+#         print('num2 is second highest')
+
+# 6) WAP to find the second lowest among n1,  n2 , n3 and n4
+
+
+
+
+
