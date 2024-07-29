bd3132731314cf0d6cc04a00f8156aab03da47ac
bhavana0403 <bhavanabm97@gmail.com>
2024-07-29 19:43:15 +0530

    looping statement

--- 5_Control_Statements/2_Looping_Statement/while_loop.py
+++ 5_Control_Statements/2_Looping_Statement/while_loop.py
@@ -187,3 +187,63 @@ while i < len(nums):
     i += 1
 print(res)
 
+# 18) WAP to extract all the string data-items in a given tuple to another tuple
+
+t = ('hai', 'universe', 90, 9.23, False, 'True')
+out = ()
+i = 0
+while i < len(t):
+    if type(t[i]) == str:
+        out += (t[i],)
+    i += 1
+print(out)
+
+# 19) WAP to create a list of all the numbers between 1 and 200 which are divisible by 3
+
+# 20) WAP to count all the numbers between 105 and 3000 which are divisible by both 5 and 7
+
+# 21) WAP to separate all the even numbers and odd numbers into 2 lists from a given homogenous list collection
+
+# 22) WAP to separate all the characters from a given string
+
+# 23) WAP to reverse a given string without using slicing
+
+# method 1
+st = 'alex'
+rev = ''
+i = len(st) - 1
+while i >= 0:
+    rev = rev + st[i]
+    i -= 1
+print(f"The reverse of a given string '{st}' is '{rev}'")
+
+"""
+st = 'alex'
+rev = '' -> 'x' -> 'xe' -> 'xel' -> 'xela'
+i = 3 -> 2 -> 1 -> 0 -> -1
+
+3 >= 0
+rev = '' + 'x' = 'x'
+
+2 >= 0
+rev = 'x' + 'e' = 'xe'
+
+1 >= 0
+rev = 'xe' + 'l' = 'xel'
+
+0 >= 0
+rev = 'xel' + 'a' = 'xela'
+
+-1 >= 0 -> False
+"""
+
+# method 2
+st = 'alex'
+rev = ''
+i = -1
+while i >= -len(st):
+    rev = rev + st[i]
+    i -= 1
+print(f"The reverse of a given string '{st}' is '{rev}'")
+
+
