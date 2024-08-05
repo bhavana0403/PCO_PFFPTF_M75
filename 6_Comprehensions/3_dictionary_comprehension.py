ac52b4d01fb94e4c6d22ee3bbe168b1a0ec40168
bhavana0403 <bhavanabm97@gmail.com>
2024-08-05 20:56:48 +0530

    dictionary comprehension

--- 6_Comprehensions/3_dictionary_comprehension.py
+++ 6_Comprehensions/3_dictionary_comprehension.py
@@ -1,4 +1,3 @@
-# dictionary comprehension
 
 st = 'hello'
 for i in enumerate(st):
@@ -64,5 +63,41 @@ C c 3
 The number of iterations of for loop when we use 'zip' will be equal to length of 
 smallest collection
 """
+
+#######################################################################################
+
+# dictionary comprehension
+"""
+Syntax:
+        {key: value     for_loop}
+"""
+# build a dictionary of word and length pair in the sentence
+sentence = "the wings of fire"
+word_len = {word: len(word) for word in sentence.split()}
+print(word_len)     # {'the': 3, 'wings': 5, 'of': 2, 'fire': 4}
+
+# flip the keys and values of the dictionary
+d = {'the': 3, 'wings': 5, 'of': 2, 'fire': 4}
+res = {d[key]: key for key in d}
+print(res)      # {3: 'the', 5: 'wings', 2: 'of', 4: 'fire'}
+
+res = {value: key for key, value in d.items()}
+print(res)      # {3: 'the', 5: 'wings', 2: 'of', 4: 'fire'}
+
+# word count
+sentence = "hello world welcome to python world hello everyone this is python world"
+print(sentence.split().count('hello'))      # 2
+
+res = {word: sentence.split().count(word) for word in sentence.split()}
+print(res)      # {'hello': 2, 'world': 3, 'welcome': 1, 'to': 1, 'python': 2, 'everyone': 1, 'this': 1, 'is': 1}
+
+words = sentence.split()
+res = {word: words.count(word) for word in words}
+print(res)      # {'hello': 2, 'world': 3, 'welcome': 1, 'to': 1, 'python': 2, 'everyone': 1, 'this': 1, 'is': 1}
+
+# count the number of times each character is repeated
+st = 'mississippi'
+res = {ch: st.count(ch) for ch in st}
+print(res)      # {'m': 1, 'i': 4, 's': 4, 'p': 2}
 
 
