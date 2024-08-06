e56f925d53d94011838fafb496067bb7a5733dd6
bhavana0403 <bhavanabm97@gmail.com>
2024-08-06 19:39:01 +0530

    dictionary comprehension

--- 6_Comprehensions/3_dictionary_comprehension.py
+++ 6_Comprehensions/3_dictionary_comprehension.py
@@ -70,6 +70,7 @@ smallest collection
 """
 Syntax:
         {key: value     for_loop}
+        {key: value  for_loop  if <cond>}
 """
 # build a dictionary of word and length pair in the sentence
 sentence = "the wings of fire"
@@ -99,5 +100,71 @@ print(res)      # {'hello': 2, 'world': 3, 'welcome': 1, 'to': 1, 'python': 2, '
 st = 'mississippi'
 res = {ch: st.count(ch) for ch in st}
 print(res)      # {'m': 1, 'i': 4, 's': 4, 'p': 2}
+
+# dictionary of characters and it's ascii value
+st = 'abcABC'
+res = {ch: ord(ch) for ch in st}
+print(res)      # {'a': 97, 'b': 98, 'c': 99, 'A': 65, 'B': 66, 'C': 67}
+
+# create a dictionary of index and value pair
+
+li = ['hai', 'everyone', 'good', 'evening']
+res1 = {index: li[index] for index in range(len(li))}
+res2 = {index: val for index, val in enumerate(li)}
+print(res1, res2, sep='\n')
+"""
+{0: 'hai', 1: 'everyone', 2: 'good', 3: 'evening'}
+{0: 'hai', 1: 'everyone', 2: 'good', 3: 'evening'}
+"""
+
+####################################################################################
+
+# word length pair only if the word is a palindrome
+st = "hello madam do you know malayalam"
+res = {word: len(word) for word in st.split() if word == word[::-1]}
+print(res)      # {'madam': 5, 'malayalam': 9}
+
+# WAP to map 2 iterables in the form of dictionary
+
+st = 'abcd'
+li = [1, 2, 3, 4]
+res = {ch: num for ch, num in zip(st, li)}
+print(res)      # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+#####################################################################################
+# build a dictionary whose value is more than 200
+
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.65,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+res = {company: price for company, price in prices.items() if price >= 200}
+print(res)      # {'AAPL': 612.65, 'IBM': 205.55}
+
+# build a dictionary whose price is less than 50
+
+#####################################################################################
+
+dial_codes = [
+    (86, 'China'),
+    (91, 'India'),
+    (44, 'United Kingdom'),
+    (1, 'United States'),
+    (92, 'Pakistan')
+]
+
+# dictionary of dial code and country
+res1 = {dial_code: country for dial_code, country in dial_codes}
+print(res1)     # {86: 'China', 91: 'India', 44: 'United Kingdom', 1: 'United States', 92: 'Pakistan'}
+
+# dictionary of country and dial code
+res2 = {country: dial_code for dial_code, country in dial_codes}
+print(res2)     # {'China': 86, 'India': 91, 'United Kingdom': 44, 'United States': 1, 'Pakistan': 92}
+
+
 
 
