e3a48309177c77fd41443b92cfaeed82b968f27f
bhavana0403 <bhavanabm97@gmail.com>
2024-08-07 19:47:13 +0530

    functions

--- 7_Functions/1_functions.py
+++ 7_Functions/1_functions.py
@@ -28,9 +28,28 @@ User Defined Functions are classified into 4 types
 
 2) Function with arguments and without return values
 -> Syntax:
+            def fname(Var1, Var2,...., Varn):
+                SB
+            fname(Val1, Val2, ...., Valn)
 
 3) Function without arguments and with return values
+-> Syntax:
+            def fname():
+                SB
+                return values
+            print(fname())
+            res = fname()
+            print(res)
+            var1, var2, ....., varn = fname()
+
 4) Function with arguments and with return values
+-> Syntax:
+            def fname(args):
+                SB
+            print(fname(args))
+            res = fname(args)
+            print(res)
+            var1, var2, ....., varn = fname(args)
 """
 
 # WAP to reverse the given string
@@ -43,22 +62,106 @@ print(rev)
 
 # Type1
 
-def greeting():
-    print('Hello Universe')
-
-print(greeting)         # <function greeting at 0x000001DB2B8F04A0>
-print(greeting())
-"""
-Hello Universe
-None
-"""
-greeting()  # Hello Universe
-
-# def reverse1():
-#     st = input('Enter the string : ')
-#     rev = ''
-#     for ch in st:
-#         rev = ch + rev
-#     print(rev)
+# def greeting():
+#     print('Hello Universe')
 #
-# reverse1()
+# print(greeting)         # <function greeting at 0x000001DB2B8F04A0>
+# print(greeting())
+# """
+# Hello Universe
+# None
+# """
+# greeting()  # Hello Universe
+
+def reverse1():
+    st = 'hello'        # input('Enter the string : ')
+    rev = ''
+    for ch in st:
+        rev = ch + rev
+    print(rev)
+
+reverse1()
+
+# WAP to print all even numbers in a list
+
+def even_num():
+    li = [101, 22, 33, 44]
+    for num in li:
+        if num % 2 == 0:
+            print(num)
+
+even_num()
+
+
+# Type 2
+
+def sam(name, empid):
+    print(f"Hello, I'm {name}, my employee ID is {empid}")
+
+sam("John", "ABC123")
+
+st = 'hello'
+def reverse2(st):
+    rev = ''
+    for ch in st:
+        rev = ch + rev
+    print(rev)
+
+reverse2(st)        # 'olleh'
+a = 'python'
+reverse2(a)         # nohtyp -> reverse2('python')
+reverse2("manual")  # launam
+
+# Type 4 - with args, with return value
+
+def calc(a, b):
+    return a+b, a-b, a*b, a/b
+
+calc(4, 2)
+print(calc(4, 2))       # (6, 2, 8, 2.0)
+res = calc(4, 2)
+print(res)      # (6, 2, 8, 2.0)
+
+add, sub, mul, div = calc(4, 2)
+print(add, sub, mul, div, sep='\n')
+"""
+6
+2
+8
+2.0
+"""
+
+# l = [1, 2, 3, 2, 2, 2]
+# l.count(2)
+# print(l.count(2))   # 4
+#
+# 4
+# print(4)
+#
+# l.append(10)
+# print(l.append(10))     # None
+
+
+def reverse4(st):
+    rev = ''
+    for ch in st:
+        rev = ch + rev
+    return rev
+
+print(reverse4('hello'))
+
+
+# Type 3
+
+def reverse3():
+    st = 'hello'
+    rev = ''
+    for i in st:
+        rev = i + rev
+    return rev
+
+print(reverse3())   # olleh
+res = reverse3()
+print(res)
+
+
