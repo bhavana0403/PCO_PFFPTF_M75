ba346db40d0af1c6af064357f0d247e629214560
bhavana0403 <bhavanabm97@gmail.com>
2024-08-10 20:10:14 +0530

    nested loops

--- 7_Functions/2_nested_functions.py
+++ 7_Functions/2_nested_functions.py
@@ -44,6 +44,27 @@ def result(li):
 
 result(words)       # ['oo', 'eei', 'o', 'ae', 'ou']
 
+#####################################################################################
+
+# WAP to get the following output
+
+nums = [86, 345, 7, 89, 26, 957]
+# o/p = [68, 543, 7, 98, 62, 759]
+
+def reverse(num):
+    rev = 0
+    while num != 0:
+        rev = rev * 10 + num % 10
+        num = num // 10
+    return rev
+
+def result(li):
+    res = []
+    for num in li:
+        res += [reverse(num)]
+    print(res)
+
+result(nums)        # [68, 543, 7, 98, 62, 759]
 
 
 
