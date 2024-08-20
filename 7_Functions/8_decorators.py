c35f8dc9c56cbaf2a3aee521ae1b5b4fb6aa3b69
bhavana0403 <bhavanabm97@gmail.com>
2024-08-20 20:29:04 +0530

    decorators

--- 7_Functions/8_decorators.py
+++ 7_Functions/8_decorators.py
@@ -1,31 +1,31 @@
 # decorators
 
-def sam(some_val):
-    return some_val
-
-print(sam(10))      # 10
-print(sam('hello'))     # hello
-
-a = 'hello world'
-b = sam(a)
-print(b)    # hello world
-
-def add(a, b):
-    return a + b
-
-print(add)      # <function add at 0x0000021A95E0AF20>
-print(add(10, 20))      # 30
-
-print(id(add))  # 2191515692832
-print(print)    # <built-in function print>
-
-a = sam(add)
-print(a)        #  <function add at 0x0000021A95E0AF20>
-print(a(10, 20))        # 30
-
-b = add
-print(b)        # <function add at 0x0000021A95E0AF20>
-print(b(10, 20))    # 30
+# def sam(some_val):
+#     return some_val
+#
+# print(sam(10))      # 10
+# print(sam('hello'))     # hello
+#
+# a = 'hello world'
+# b = sam(a)
+# print(b)    # hello world
+#
+# def add(a, b):
+#     return a + b
+#
+# print(add)      # <function add at 0x0000021A95E0AF20>
+# print(add(10, 20))      # 30
+#
+# print(id(add))  # 2191515692832
+# print(print)    # <built-in function print>
+#
+# a = sam(add)
+# print(a)        #  <function add at 0x0000021A95E0AF20>
+# print(a(10, 20))        # 30
+#
+# b = add
+# print(b)        # <function add at 0x0000021A95E0AF20>
+# print(b(10, 20))    # 30
 
 ######################################################################################
 
@@ -34,21 +34,21 @@ def sam(a):
         print(a)
     return inner
 
-li = [10, 20, 30]
-print(li)       # [10, 20, 30]
-
-a = 10
+# li = [10, 20, 30]
+# print(li)       # [10, 20, 30]
+#
+# a = 10
 # print(inner())  # NameError
 
-print(sam(12))      # <function sam.<locals>.inner at 0x000001F1DB7604A0>
-
-b = sam(100)
-c = sam(200)
-print(b)        # <function sam.<locals>.inner at 0x000001D3A57304A0>
-print(c)        # <function sam.<locals>.inner at 0x000001D3A596F600>
-
-b()      # 100
-c()      # 200
+# print(sam(12))      # <function sam.<locals>.inner at 0x000001F1DB7604A0>
+#
+# b = sam(100)
+# c = sam(200)
+# print(b)        # <function sam.<locals>.inner at 0x000001D3A57304A0>
+# print(c)        # <function sam.<locals>.inner at 0x000001D3A596F600>
+#
+# b()      # 100
+# c()      # 200
 
 #####################################################################################
 
@@ -68,17 +68,17 @@ def demo():
     return "In Demo"
 
 
-a = sam(greeting)
-b = sam(demo)
-print(a)    # <function sam.<locals>.inner at 0x0000020EF3ACF880>
-print(b)    # <function sam.<locals>.inner at 0x0000020EF3ACF920>
+# a = sam(greeting)
+# b = sam(demo)
+# print(a)    # <function sam.<locals>.inner at 0x0000020EF3ACF880>
+# print(b)    # <function sam.<locals>.inner at 0x0000020EF3ACF920>
 
-print(a())
+# print(a())
 """
 <function greeting at 0x000001CAB5B3F600>
 Hello World
 """
-print(b())
+# print(b())
 """
 <function demo at 0x000001CAB5B3F7E0>
 In Demo
@@ -104,18 +104,18 @@ def demo():
 # a = sam(greeting)
 # print(a)        # <function sam.<locals>.inner at 0x000001B73566F9C0>
 
-greeting = sam(greeting)
-print(greeting)     # <function sam.<locals>.inner at 0x000001869E3FF9C0>
+# greeting = sam(greeting)
+# print(greeting)     # <function sam.<locals>.inner at 0x000001869E3FF9C0>
 
-demo = sam(demo)
-print(demo)     # <function sam.<locals>.inner at 0x0000021956DEFA60>
+# demo = sam(demo)
+# print(demo)     # <function sam.<locals>.inner at 0x0000021956DEFA60>
 
-print(greeting())
+# print(greeting())
 """
 <function greeting at 0x000001EFB2CDF6A0>
 Hello World
 """
-print(demo())
+# print(demo())
 """
 <function demo at 0x000001EFB2CDF920>
 In Demo
@@ -138,13 +138,13 @@ def sub(x, y):
     return x - y
 
 
-add = calculate(add)
-print(add)      # <function calculate.<locals>.wrapper at 0x0000017DF977FC40>
-
-sub = calculate(sub)
-print(sub)      # <function calculate.<locals>.wrapper at 0x0000017DF977FCE0>
-
-print(add(3, 4))    # 7
+# add = calculate(add)
+# print(add)      # <function calculate.<locals>.wrapper at 0x0000017DF977FC40>
+#
+# sub = calculate(sub)
+# print(sub)      # <function calculate.<locals>.wrapper at 0x0000017DF977FCE0>
+#
+# print(add(3, 4))    # 7
 
 #######################################################################################
 
@@ -163,14 +163,14 @@ def sub(x, y):
     return x - y
 
 
-add = calculate(add)
-print(add)      # <function calculate.<locals>.wrapper at 0x0000017DF977FC40>
-
-sub = calculate(sub)
-print(sub)      # <function calculate.<locals>.wrapper at 0x0000017DF977FCE0>
-
-print(add(3, 4, 8))
-print(sub(20, 7))
+# add = calculate(add)
+# print(add)      # <function calculate.<locals>.wrapper at 0x0000017DF977FC40>
+#
+# sub = calculate(sub)
+# print(sub)      # <function calculate.<locals>.wrapper at 0x0000017DF977FCE0>
+#
+# print(add(3, 4, 8))
+# print(sub(20, 7))
 
 ######################################################################################
 
@@ -217,6 +217,28 @@ def sub(x, y):
 """
 -> instead of writing this we write @decorator_name before creating the main function
 """
-print(add(10, 5, 7))
-print(sub(10, 5))
+# print(add(10, 5, 7))
+# print(sub(10, 5))
 
+######################################################################################
+
+# 1) decorator to print the name of the function which is executed
+
+def print_func_name(func):
+    def wrapper(*args, **kwargs):
+        print(f"{func.__name__} function will be executed")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
+
+@print_func_name        # sam = print_func_name(sam)
+def sam(*args, **kwargs):
+    return args, kwargs
+
+@print_func_name        # demo = print_func_name(demo)
+def demo():
+    return "In demo"
+
+
+print(demo())
+print(sam(1, 2, a=10))
