5532019a1715c824b4af79c0d35cbe130487405b
bhavana0403 <bhavanabm97@gmail.com>
2024-08-31 19:44:43 +0530

    inheritance

--- 11_OOPS/6_inheritance.py
+++ 11_OOPS/6_inheritance.py
@@ -45,26 +45,70 @@ Single Level Inheritance
 #
 # print(dir(A))
 
+# class Demo:
+#     a = 10
+#     b = 20
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+
+# class Child_Demo(Demo):
+#     pass
+
+# print(dir(Demo))
+# print(dir(Child_Demo))   # same as that of dir(Demo)
+#
+# print(Demo.__dict__)        #  'a': 10, 'b': 20,
+# print(Child_Demo.__dict__)
+#
+# c1 = Child_Demo(9, 8)
+# print(c1.__dict__)      # {'x': 9, 'y': 8}
+# print(dir(c1))      # 'a', 'b', 'x', 'y'
+
+####################################################################################
+
+# child and parent has unique properties
+
 class Demo:
-    a = 10
-    b = 20
+    def __init__(self, value):
+        self.value = value
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def apple(self):
+        print('Demo.apple', self.value)
+
+    def yahoo(self):
+        print('Demo.yahoo', self.value)
 
 
-class Child_Demo(Demo):
-    pass
+class Demo1(Demo):
+    def google(self):
+        print('Demo1.google', self.value)
 
-print(dir(Demo))
-print(dir(Child_Demo))   # same as that of dir(Demo)
 
-print(Demo.__dict__)        #  'a': 10, 'b': 20,
-print(Child_Demo.__dict__)
+obj1 = Demo(11)
+obj2 = Demo1(22)
 
-c1 = Child_Demo(9, 8)
-print(c1.__dict__)      # {'x': 9, 'y': 8}
-print(dir(c1))      # 'a', 'b', 'x', 'y'
+print(dir(Demo))        # 'apple', 'yahoo'
+print(Demo.__dict__)    # apple, yahoo, __init__
+
+print(dir(Demo1))       # 'apple', 'google', 'yahoo'
+print(Demo1.__dict__)   # 'google'
+
+print(dir(obj1))        # 'apple', 'value', 'yahoo'
+print(obj1.__dict__)    # {'value': 11}
+
+print(dir(obj2))        # 'apple', 'google', 'value', 'yahoo'
+print(obj2.__dict__)    # {'value': 22}
+
+
+
+
+
+
+
+
+
 
 
