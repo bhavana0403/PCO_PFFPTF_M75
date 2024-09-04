54ac17632118ab3644b37c2ae9d435c90133db57
bhavana0403 <bhavanabm97@gmail.com>
2024-09-04 20:40:05 +0530

    magic methods

--- 11_OOPS/10_magic_methods.py
+++ 11_OOPS/10_magic_methods.py
@@ -45,3 +45,60 @@ print(p1 + p2)
 
 ################################################################################
 
+# 2) comparison protocol
+""" ==, !=, <, >, <=, >= """
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __gt__(self, other):
+        # return self.x > other.x and self.y > other.y
+        # return self.x > other.x or self.y > other.y
+        # return self.x > other.x
+        return self.x + self.y > other.x + other.y
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+
+# p1 = Point(3, 9)
+# p2 = Point(10, 5)
+# p3 = Point(100, 200)
+# print(p1 < p2)
+# print(p1 < p3)      # p3.__gt__(p1)
+#
+# p4 = Point(10, 80)
+# print(p2 == p4)
+# print(p2 != p4)
+
+
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def __eq__(self, other):
+        # return self.age == other.age
+        # return self.salary == other.salary
+        return self.age == other.age and self.salary == other.salary
+
+    def __lt__(self, other):
+        return self.salary < other.salary
+
+
+# e1 = Employee("John", 24, 40000)
+# e2 = Employee("Mary", 24, 56000)
+# e3 = Employee("Abhi", 40, 56000)
+# e4 = Employee("Ram", 40, 56000)
+#
+# print(e1 == e2)
+# print(e3 == e4)
+#
+# print(e1 < e3)
+
+###################################################################################
+
+# 3) container protocol
