b7b44ecfae405117349ddc571f3a8323ce45ce59
bhavana0403 <bhavanabm97@gmail.com>
2024-09-17 20:50:24 +0530

    exception handling

--- 13_Exception_Handling/exception_handling.py
+++ 13_Exception_Handling/exception_handling.py
@@ -122,7 +122,128 @@ def add():
 #             print(msg)
 # add()
 
+"""The above program can handle only TypeError but not other errors, we can 
+create multiple except blocks to handle different types of errors"""
 
+# def add():
+#     try:
+#         a = eval(input('Enter the value of a : '))
+#         b = eval(input('Enter the value of b : '))
+#         print(a + b)
+#     except TypeError as msg:
+#         if type(a) == set and type(b) == set:
+#             print(a.union(b))
+#         elif type(a) == dict and type(b) == dict:
+#             print({**a, **b})
+#         else:
+#             print(msg)
+#             add()
+#     except SyntaxError:
+#         print("Syntax Error handled")
+#         add()
+#
+#
+# add()
+
+
+# def add():
+#     try:
+#         a = int(input())
+#         b = int(input())
+#         print(a + b)
+#     except ValueError as msg:
+#         print(msg)
+#         add()
+#
+# add()
+#
+# a = eval(input())
+# b = eval(input())
+# print(a / b)
+
+# def div():
+#     try:
+#         a = eval(input())
+#         b = eval(input())
+#         print(a / b)
+#     except TypeError as message:
+#         print(message)
+#         print('TypeError Handled')
+#     except ZeroDivisionError as message:
+#         print(message)
+#         print('ZeroDivisionError Handled')
+#     except SyntaxError:
+#         print('SyntaxError Handled')
+#
+# div()
+
+
+# 2) Generic Exception Handling
+"""
+-> Syntax:
+        try:
+            Problem Statement
+        except Exception/ BaseException as msg:
+            Solution
+"""
+
+# def div():
+#     try:
+#         a = eval(input())
+#         b = eval(input())
+#         print(a / b)
+#     except Exception as msg:
+#         print(msg)
+#
+# div()
+
+
+# l = [10, 20, 30]
+# l.remove(90)    # ValueError
+
+# try:
+#     l = [10, 20, 30]
+#     l.remove(90)
+# except BaseException:
+#     print('Error Handled')
+
+# while True:
+#     print('Hello')
+
+# try:
+#     while True:
+#         print('Hello')
+# except Exception:
+#     print('Error Handled')
+
+"""
+KeyboardInterrupt can not be handled by generic exception block
+"""
+
+##############################################################################
+
+# 3) Default Exception Handling
+"""
+-> It can handle all kinds of exception including Keyboard Interruption
+-> Syntax:
+            try:
+                Problem Statement
+            except:
+                Solution Block
+"""
+
+# try:
+#     while True:
+#         print('Hello')
+# except:
+#     print('Exception Handled')
+
+# try:
+#     print(10 / 0)
+# except:
+#     print('Error Handled')
+#
+# print('Hello')
 
 
 
