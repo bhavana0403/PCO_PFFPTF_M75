ae5490fd4eac657311fe20d220bbf61af0b3bb0f
bhavana0403 <bhavanabm97@gmail.com>
2024-09-18 20:00:21 +0530

    exception handling

--- 13_Exception_Handling/exception_handling.py
+++ 13_Exception_Handling/exception_handling.py
@@ -288,23 +288,49 @@ def div():
 class PhoneLocked(Exception):
     pass
 
-for i in range(3):
-    try:
-        actual_password = '1234'
-        entered_password = input('Enter the password : ')
-        if actual_password == entered_password:
-            print('Phone Unlocked')
-            break
-        elif entered_password.isdigit() == False:
-            raise TypeError("Password must contain only digits")
-        elif len(entered_password) != len(actual_password):
-            raise ValueError("Password has exactly 4 digits")
-        else:
-            raise ValueError("Incorrect Password")
-    except ValueError as message:
-        print(message)
-    except TypeError as message:
-        print(message)
+# for i in range(3):
+#     try:
+#         actual_password = '1234'
+#         entered_password = input('Enter the password : ')
+#         if actual_password == entered_password:
+#             print('Phone Unlocked')
+#             break
+#         elif entered_password.isdigit() == False:
+#             raise TypeError("Password must contain only digits")
+#         elif len(entered_password) != len(actual_password):
+#             raise ValueError("Password has exactly 4 digits")
+#         else:
+#             raise ValueError("Incorrect Password")
+#     except ValueError as message:
+#         print(message)
+#     except TypeError as message:
+#         print(message)
+# else:
+#     raise PhoneLocked ("No of attempts exceeded")
+
+###############################################################################
+
+try:
+    a = eval(input())
+    b = eval(input())
+    print(a + b)
+except TypeError:
+    print('TypeError Handled')
+except Exception:
+    print('Generic Exception Handled')
+except:
+    print('Default Exception Handled')
 else:
-    raise PhoneLocked ("No of attempts exceeded")
+    print('In else Block')
+finally:
+    print('In finally block')
+
+"""
+-> else - when there are no exceptions, else block gets executed
+-> finally - gets executed even if there is error or no error
+-> Order 
+        try -> specific -> generic -> default -> else -> finally
+"""
+
+
 
